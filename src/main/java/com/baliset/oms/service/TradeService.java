47185e1d4d80f34945372735f67c6afa0b053f53
Haim Zamir <hzamir@gmail.com>
2017-09-19 22:45:54 -0400

    tradeservice has several list queries

--- src/main/java/com/baliset/oms/service/TradeService.java
+++ src/main/java/com/baliset/oms/service/TradeService.java
@@ -5,14 +5,53 @@ import org.slf4j.*;
 import org.springframework.stereotype.*;
 
 import java.util.*;
+import java.util.stream.*;
 
 @Service
 public class TradeService
 {
   private static Logger logger = LoggerFactory.getLogger(TradeService.class);
+  private ArrayList<TradeEx> tradeHistory = new ArrayList<>(1000);
+
+  @SuppressWarnings(value = "unchecked")
+  public  List<TradeEx> listAll()
+  {
+    return (List<TradeEx>)tradeHistory.clone();
+  }
+
+  public List<TradeEx> listAll(long from, long to)
+  {
+    return tradeHistory.stream()
+                       .filter(tradeEx -> tradeEx.getTimestamp() >= from && tradeEx.getTimestamp() < to)
+                       .collect(Collectors.toList());
+
+  }
+
+  public List<TradeEx> list(String symbol)
+  {
+    return tradeHistory.stream()
+                       .filter(tradeEx -> tradeEx.getSymbol().equals(symbol))
+                       .collect(Collectors.toList());
+  }
+
+  public List<TradeEx> list(String symbol, long from, long to)
+  {
+    return tradeHistory.stream()
+                       .filter(tradeEx -> tradeEx.getSymbol().equals(symbol))
+                       .filter(tradeEx -> tradeEx.getTimestamp() >= from && tradeEx.getTimestamp() < to)
+                       .collect(Collectors.toList());
+
+  }
 
   public void reportTrades(List<TradeEx> trades)
   {
-    trades.stream().forEach(tradeEx -> logger.info(tradeEx.toString()));
+    addToHistory(trades);
+    trades.forEach(tradeEx -> logger.info(tradeEx.toString()));
+  }
+
+
+  private void addToHistory(List<TradeEx> trades)
+  {
+    tradeHistory.addAll(trades);
   }
 }
