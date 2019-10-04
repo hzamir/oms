4d0bf0aacdbb4675d98a4421e40a909bcecb5203
hzamir <hzamir@gmail.com>
2019-10-03 21:46:07 -0400

    first kotlin conversion attempt

--- src/main/java/com/baliset/Application.kt
+++ src/main/java/com/baliset/Application.kt
@@ -1,36 +1,36 @@
-package com.baliset;
+package com.baliset
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.context.ApplicationContext;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.*
+import org.springframework.context.ApplicationContext
+import org.springframework.scheduling.annotation.EnableScheduling
 
 
 @EnableScheduling
 @SpringBootApplication
-public class Application
-{
-  private static Logger logger = LoggerFactory.getLogger(Application.class);
+open class Application {
 
-  ApplicationContext applicationContext;
+    internal lateinit var applicationContext: ApplicationContext
+    //do as much as possible inside an instance of Application rather than in a static method
+    private fun stuff() {
+        logger.info("Kudos, the server is ready") // todo: replace with pattern that checks all parts are ready
 
-  public static void main(String[] args)
-  {
-    ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
 
-    Application application = ctx.getBean(Application.class);  // the application might as well be a singleton too, no?
-    application.applicationContext = ctx;  // todo: can this be injected straight into Application instance
+    companion object {
+        private val logger = LoggerFactory.getLogger(Application::class.java)
 
-    application.stuff();                   // todo: maybe just use initializationaware lifecycle routine for this?
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val ctx = SpringApplication.run(Application::class.java, *args)
 
-  }
-  //do as much as possible inside an instance of Application rather than in a static method
-  private void stuff()
-  {
-    logger.info("Kudos, the server is ready"); // todo: replace with pattern that checks all parts are ready
+            val application = ctx.getBean<Application>(Application::class.java)  // the application might as well be a singleton too, no?
+            application.applicationContext = ctx  // todo: can this be injected straight into Application instance
 
-  }
+            application.stuff()                   // todo: maybe just use initializationaware lifecycle routine for this?
+
+        }
+    }
 }
